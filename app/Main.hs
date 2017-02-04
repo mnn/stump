224925dfae529baadfaa01e2b20af4a7899bc943
monnef <not@s.et>
2017-02-04 17:38:30 +0100

    Finished elem to article preview function, first stab at JSON serialization.

--- app/Main.hs
+++ app/Main.hs
@@ -3,18 +3,28 @@
 module Main where
 
 import           Control.Monad.Trans.Class
-import           Data.Monoid               ((<>))
-import qualified Data.Text                 as T
-import qualified Data.Text.Lazy            as TL
+import           Data.Aeson                 (encode, FromJSON, ToJSON)
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Char8      as C
+import qualified Data.ByteString.Lazy       as BL
+import qualified Data.ByteString.Lazy.Char8 as CL
+import           Data.List
+import           Data.Monoid                ((<>))
+import qualified Data.Text                  as T
+import qualified Data.Text.Lazy             as TL
 import           Lib
 import           Test.WebDriver
 import           Test.WebDriver.Session
 import           Web.Scotty.Trans
 
+encodeToString :: ToJSON a => a -> String
+encodeToString = CL.unpack . encode
+
 articlePreviews :: ActionT TL.Text WD ()
 articlePreviews = do
-  a <- lift extractArticlePreviews
-  text $ "got this for you:\n" <> TL.pack (show a)
+  previews <- lift extractArticlePreviews
+  let a = intercalate "\n" $ map encodeToString previews
+  text $ "got this for you:\n" <> TL.pack a
 
 routes :: ScottyT TL.Text WD ()
 routes = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric          #-}
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses  #-}
@@ -10,10 +11,13 @@ module Lib where
 import           Control.Lens
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
+import           Data.Aeson                (FromJSON, ToJSON)
 import           Data.Maybe
 import           Data.Monoid               ((<>))
+import           Data.Text
 import qualified Data.Text                 as T
 import qualified Data.Text.Lazy            as TL
+import           GHC.Generics
 import           Safe
 import           Test.WebDriver
 import           Test.WebDriver.Class
@@ -21,14 +25,19 @@ import           Test.WebDriver.Commands
 
 data ArticlePreview = ArticlePreview
   { _articlePreviewName          :: T.Text
-  , _articlePreviewPreview       :: T.Text
+  , _articlePreviewPerex         :: T.Text
   , _articlePreviewAuthorName    :: T.Text
   , _articlePreviewAuthorUrl     :: T.Text
   , _articlePreviewDate          :: T.Text
   , _articlePreviewCategoryName  :: T.Text
   , _articlePreviewCategoryUrl   :: T.Text
-  , _articlePreviewCommentsCount :: Integer
-  } deriving (Show, Eq)
+  , _articlePreviewCommentsCount :: Maybe Integer
+  } deriving (Show, Eq, Generic)
+
+makeFields ''ArticlePreview
+
+instance ToJSON ArticlePreview
+instance FromJSON ArticlePreview
 
 firefoxConfig :: WDConfig
 firefoxConfig = defaultConfig
@@ -53,19 +62,37 @@ getTextFromDef e sel def = do
   textMay <- mapM getText may
   return $ fromMaybe def textMay
 
+getUrl :: T.Text -> Element -> WD T.Text
+getUrl def e = do
+  may <- attr e "href"
+  return $ fromMaybe def may
+
+getUrlFromDef :: Element -> T.Text -> T.Text -> WD T.Text
+getUrlFromDef e sel def = do
+  may <- findElemFromMay e sel
+  textMay <- mapM (getUrl def) may
+  return $ fromMaybe def textMay
+
 elemToArticlePreview :: Element -> WD ArticlePreview
 elemToArticlePreview e = do
-  name <- getTextFromDef e ".article__heading.article__heading--content" "<Failed to get name>"
-  -- TODO: extract more article fields
+  name <- getTextFromDef e ".article__heading" "<Failed to get name>"
+  perex <- getTextFromDef e ".perex" "<Failed to get perex>"
+  author <- getTextFromDef e ".impressum__author" "<Failed to get author>"
+  authorUrl <- getUrlFromDef e ".impressum__author" "<Failed to get author url>"
+  date <- getTextFromDef e ".impressum__date" "<Failed to get date>"
+  category <- getTextFromDef e ".impressum__rubric" "<Failed to get category>"
+  categoryUrl <- getUrlFromDef e ".impressum__rubric" "<Failed to get category>"
+  commentsCountStr <- getTextFromDef e ".comments__count" "<Failed to get comments count>"
+  let commentsCount = readMay $ T.unpack commentsCountStr
   return ArticlePreview {
     _articlePreviewName = name
-  , _articlePreviewPreview = ""
-  , _articlePreviewAuthorName = ""
-  , _articlePreviewAuthorUrl = ""
-  , _articlePreviewDate = ""
-  , _articlePreviewCategoryName = ""
-  , _articlePreviewCategoryUrl = ""
-  , _articlePreviewCommentsCount= -1
+  , _articlePreviewPerex = perex
+  , _articlePreviewAuthorName = author
+  , _articlePreviewAuthorUrl = authorUrl
+  , _articlePreviewDate = date
+  , _articlePreviewCategoryName = category
+  , _articlePreviewCategoryUrl = categoryUrl
+  , _articlePreviewCommentsCount = commentsCount
   }
 
 extractArticlePreviews :: WD [ArticlePreview]
